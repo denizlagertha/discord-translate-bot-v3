57931ef69fa3689a0bb854897b94b8dc1e7dfa88
denizlagertha <nadirenh@gmail.com>
2026-01-10 03:30:14 +0300

    main.py

--- main.py
+++ main.py
@@ -6,8 +6,6 @@ from discord import app_commands
 from keep_alive import keep_alive
 
 TOKEN = os.getenv("TOKEN")
-RAPIDAPI_KEY = os.getenv("RAPIDAPI_KEY")
-
 LANG = {}  # Server language
 
 intents = discord.Intents.default()
@@ -24,20 +22,14 @@ language_options = {
 }
 
 def translate(text, target):
-    url = "https://deep-translate1.p.rapidapi.com/language/translate/v2"
-    payload = {"text": text, "target_language": target}
-    headers = {
-        "x-rapidapi-key": RAPIDAPI_KEY,
-        "x-rapidapi-host": "deep-translate1.p.rapidapi.com"
-    }
-
-    r = requests.post(url, json=payload, headers=headers)
+    url = f"https://api.mymemory.translated.net/get?q={text}&langpair=auto|{target}"
+    r = requests.get(url)
 
     if r.status_code == 200:
-        return r.json()["data"]["translations"]["translatedText"]
+        data = r.json()
+        return data["responseData"]["translatedText"]
     else:
         return "translation failed"
-
 
 @client.tree.command(name="setlang", description="Set server language")
 @app_commands.choices(lang=[
@@ -52,7 +44,6 @@ async def translate_context(interaction: discord.Interaction, message: discord.M
     target = LANG.get(interaction.guild_id, "en")
     translated = translate(message.content, target)
     await interaction.response.send_message(f"🔤 **{translated}**", ephemeral=True)
-
 
 @client.event
 async def on_ready():
